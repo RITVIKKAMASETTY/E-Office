19c5c27f543bb0cf22056fae47cb3361267e7b22
Khushbu-github <khushbuhajari530@gmail.com>
2025-10-14 12:00:11 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import TeamLeadDashboard from './components/teamlead/TeamLead';
 import EmployeeDashboard from './components/employee/EmployeeDashboard';
+<<<<<<< Updated upstream
 import ManagerDashboard from './pages/Manager';
+=======
+import AdminDashboard from './components/admin/Admin';
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
@@ -15,9 +19,16 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard/teamlead" element={<TeamLeadDashboard />} />
           {/* Add other dashboard routes as needed */}
+<<<<<<< Updated upstream
           <Route path="/dashboard/admin" element={<div>Admin Dashboard</div>} />
           <Route path="/dashboard/manager" element={<ManagerDashboard />} />
+=======
+          <Route path="/dashboard/admin" element={<AdminDashboard />} />
+          {/* <Route path="/dashboard/admin" element={<div>Admin Dashboard</div>} /> */}
+          <Route path="/dashboard/manager" element={<div>Manager Dashboard</div>} />
+>>>>>>> Stashed changes
           <Route path="/dashboard/member" element={<EmployeeDashboard />} />
+
           <Route path="/dashboard" element={<div>Default Dashboard</div>} />
         </Routes>
       </div>
